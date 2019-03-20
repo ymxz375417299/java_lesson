d6a4cb83a4113f8e819c856f5731e0002038999f
rock <ymxz34787409@126.com>
2019-03-20 15:51:35 +0800

    添加day01代码

---
+++ day01/Demo02.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Demo02{
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+      System.out.println("请输入第一个数字");
+      int a = scanner.nextInt();
+      System.out.println("请在输入一个数");
+      int b = scanner.nextInt();
+      System.out.println("输入的两个数是否相等：" + (a == b));
+
+  }
+}
